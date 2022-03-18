bd58dea60082ebcddae841bf904e365be4f47a56
Jesse-Unruh-Garcia <98713768+Jesse-Unruh-Garcia@users.noreply.github.com>
2022-03-17 21:21:44 -0400

    Added Max_length

--- home/models.py
+++ home/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Recipe(models.Model):
     recipeTitle = models.CharField(max_length=200)
     recipeText = models.TextField()
-    recipeIngredeients = models.CharField()
+    recipeIngredeients = models.CharField(max_length=200)
     writer = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     likes = models.IntegerField(default=0)
