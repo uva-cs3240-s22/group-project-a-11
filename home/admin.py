2c338ab91fce0eef6c5f093466a0def7c9efaf68
anrath <aklounts@gmail.com>
2022-03-23 17:23:52 -0400

    Initial model changes

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Recipe
+from .models import Recipe, Ingredient, Step
 
-admin.site.register(Recipe)
+admin.site.register([Recipe, Ingredient, Step])
--- home/models.py
+++ home/models.py
@@ -1,18 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=200)
+    units = models.CharField(max_length=200, blank=True)
+    quantity = models.IntegerField(blank=True)
+    qualifier = models.CharField(max_length=200, blank=True)
+    def __str__(self):
+        return self.name
+
+class Step(models.Model):
+    text = models.CharField(max_length=500, default="")
+    asset_url = models.CharField(max_length=200, blank=True) # Will change when we figure out images
+    def __str__(self):
+        return self.text
+
 class Recipe(models.Model):
     recipeTitle = models.CharField(max_length=200)
     recipeText = models.TextField()
-    writer = models.CharField(max_length=200)
+    writer = models.OneToOneField(User, on_delete=models.CASCADE)
     likes = models.IntegerField(default=0)
+    time_to_make = models.IntegerField(default=60)
+    steps = models.ManyToManyField(Step)
+    ingredients = models.ManyToManyField(Ingredient)
     def __str__(self) -> str:
         return self.recipeTitle
-
-class Ingredients(models.Model):
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
-    name = models.CharField(max_length=200)
-    units = models.CharField(max_length=200)
-    quantity = models.IntegerField()
-    def __str__(self):
-        return self.name
