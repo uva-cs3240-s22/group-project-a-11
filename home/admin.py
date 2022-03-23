68e1ac74de844a063d6cf2d8a2e5c349f550dcaf
anrath <aklounts@gmail.com>
2022-03-23 17:56:29 -0400

    fixing model bugs and adding forking fundamentals

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Recipe, Ingredient, Step
+from .models import Recipe, Ingredient, Step, Meal_Type, Cuisine_Type
 
-admin.site.register([Recipe, Ingredient, Step])
+admin.site.register([Recipe, Ingredient, Step, Meal_Type, Cuisine_Type])
--- home/models.py
+++ home/models.py
@@ -3,11 +3,19 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Meal_Type(models.Model):
+    name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
+class Cuisine_Type(models.Model):
+    name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
 class Ingredient(models.Model):
     name = models.CharField(max_length=200)
-    units = models.CharField(max_length=200, blank=True)
-    quantity = models.IntegerField(blank=True)
-    qualifier = models.CharField(max_length=200, blank=True)
+    units = models.CharField(max_length=200, blank=True, null=True)
+    quantity = models.IntegerField(blank=True, null=True)
+    qualifier = models.CharField(max_length=200, blank=True, null=True)
     def __str__(self):
         return self.name
 
@@ -18,12 +26,21 @@ class Step(models.Model):
         return self.text
 
 class Recipe(models.Model):
+
     recipeTitle = models.CharField(max_length=200)
     recipeText = models.TextField()
-    writer = models.OneToOneField(User, on_delete=models.CASCADE)
+    writer = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
     likes = models.IntegerField(default=0)
     time_to_make = models.IntegerField(default=60)
     steps = models.ManyToManyField(Step)
     ingredients = models.ManyToManyField(Ingredient)
+
+    meal_type = models.ManyToManyField(Meal_Type)
+    cuisine_type = models.ManyToManyField(Cuisine_Type)
+
+    parentRecipe = models.ForeignKey('self', unique=False, related_name="childrenRecipe", on_delete=models.CASCADE, null=True, blank=True)
+
+    
     def __str__(self) -> str:
         return self.recipeTitle
