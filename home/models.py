1c1de349d2edc769d1dfb7a40a372492d79fdc07
Jesse-Unruh-Garcia <98713768+Jesse-Unruh-Garcia@users.noreply.github.com>
2022-03-25 15:33:34 -0400

    So many things

--- home/models.py
+++ home/models.py
@@ -3,43 +3,21 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Meal_Type(models.Model):
-    name = models.CharField(max_length=200)
-    def __str__(self):
-        return self.name
-class Cuisine_Type(models.Model):
-    name = models.CharField(max_length=200)
-    def __str__(self):
-        return self.name
-class Ingredient(models.Model):
-    name = models.CharField(max_length=200)
-    units = models.CharField(max_length=200, blank=True, null=True)
-    quantity = models.IntegerField(blank=True, null=True)
-    qualifier = models.CharField(max_length=200, blank=True, null=True)
-    def __str__(self):
-        return self.name
-
-class Step(models.Model):
-    text = models.CharField(max_length=500, default="")
-    asset_url = models.CharField(max_length=200, blank=True) # Will change when we figure out images
-    def __str__(self):
-        return self.text
-
 class Recipe(models.Model):
-
     recipeTitle = models.CharField(max_length=200)
     recipeText = models.TextField()
     writer = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     likes = models.IntegerField(default=0)
     time_to_make = models.IntegerField(default=60)
-    steps = models.ManyToManyField(Step)
+    """steps = models.ManyToManyField(Step)
     ingredients = models.ManyToManyField(Ingredient)
 
     meal_type = models.ManyToManyField(Meal_Type)
-    cuisine_type = models.ManyToManyField(Cuisine_Type)
+    cuisine_type = models.ManyToManyField(Cuisine_Type)"""
 
-    parentRecipe = models.ForeignKey('self', unique=False, related_name="childrenRecipe", on_delete=models.CASCADE, null=True, blank=True)
+    parentRecipe = models.ForeignKey('self', unique=False, related_name="childrenRecipe", on_delete=models.CASCADE,
+                                     null=True, blank=True)
 
     BEGINNER = 'BE'
     INTERMEDIATE = 'IN'
@@ -55,6 +33,32 @@ class Recipe(models.Model):
         default=INTERMEDIATE,
     )
 
-    
     def __str__(self) -> str:
-        return self.recipeTitle
+        return str(self.recipeTitle)
+
+
+class Meal_Type(models.Model):
+    name = models.CharField(max_length=200)
+    recipe = models.ManyToManyField(Recipe)
+    def __str__(self):
+        return self.name
+class Cuisine_Type(models.Model):
+    name = models.CharField(max_length=200)
+    recipe = models.ManyToManyField(Recipe)
+    def __str__(self):
+        return self.name
+class Ingredient(models.Model):
+    name = models.CharField(max_length=200)
+    units = models.CharField(max_length=200, blank=True, null=True)
+    quantity = models.IntegerField(blank=True, null=True)
+    qualifier = models.CharField(max_length=200, blank=True, null=True)
+    recipe = models.ManyToManyField(Recipe)
+    def __str__(self):
+        return str(self.name)
+
+class Step(models.Model):
+    text = models.CharField(max_length=500, default="")
+    asset_url = models.CharField(max_length=200, blank=True) # Will change when we figure out images
+    recipe = models.ManyToManyField(Recipe)
+    def __str__(self):
+        return str(self.text)
--- wordOfMouth/urls.py
+++ wordOfMouth/urls.py
@@ -17,18 +17,17 @@ from django.contrib import admin
 from django.urls import path, include
 from home.views import home_view
 from home.views import submit_recipe
-from home.views import RecipeView
-from home.views import home_view, template_testing_view_recipe, \
-    template_testing_view_recipe_add, template_testing_view_feed
+from home.views import home_view, template_testing_view_recipe, recipeView, template_testing_view_feed, add_step, add_ingredient
 
-app_name = 'home'
+app_name = 'wordOfMouth'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path('', home_view, name='home'),
     path('recipeSubmission', submit_recipe, name='recipeSubmission'),
-    path('recipe/',RecipeView.as_view(),name="recipeView")
     path('recipe-template-testing/', template_testing_view_recipe, name="recipe_template-testing"),
-    path('recipe-add-template-testing/', template_testing_view_recipe_add, name="recipe_add_template-testing"),
+    path('recipe/<int:recipe_id>' , recipeView, name="recipe_add_template-testing"),
     path('feed-template-testing/', template_testing_view_feed, name="feed_template-testing"),
+    path('ingredAdd/<int:recipe_id>', add_ingredient, name="ingredient-adding"),
+    path('stepAdd/<int:recipe_id>', add_step, name="step-adding"),
 ]
