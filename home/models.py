2755cd8e09be72174c6f16ffc79f014b92232edf
aljariri <ma7qg@virginia.edu>
2022-03-26 14:53:10 -0400

    'favorite_view' Update
    Added more logic to the favorite_view

--- home/models.py
+++ home/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 class Recipe(models.Model):
     recipeTitle = models.CharField(max_length=200)
@@ -8,6 +9,7 @@ class Recipe(models.Model):
     writer = models.CharField(max_length=200)
     # likes = models.IntegerField(default=0)
     like = models.ManyToManyField(User, default=None, blank=True)
+
     def __str__(self) -> str:
         return self.recipeTitle
 
@@ -21,13 +23,18 @@ class Ingredients(models.Model):
     name = models.CharField(max_length=200)
     units = models.CharField(max_length=200)
     quantity = models.IntegerField()
+
     def __str__(self):
         return self.name
+
+
+for_favorite = (('Like', 'Like'), ('Unlike', 'Unlike'))
+
 
 class Favorite(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     recipe_liked = models.ForeignKey(Recipe, on_delete=models.CASCADE)
-    like_option = models.CharField(default='Like', max_length=10)
+    like_option = models.CharField(choice=for_favorite, default='Like', max_length=10)
 
     def __str__(self):
         return str(self.recipe_liked)
--- home/views.py
+++ home/views.py
@@ -6,6 +6,7 @@ from django.views import generic
 from django.utils import timezone
 from .models import Recipe, Favorite
 
+
 # Create your views here.
 def home_view(request):
     """
@@ -13,11 +14,34 @@ def home_view(request):
     """
     return render(request, "home.html", {})
 
+
 def favorite_view(request):
+    user = request.user
+    if request.method == 'POST':
+        recipe_id = request.POST.get(
+            'post_id')  # needs to be implemented in the main html (post_id needs to be defined)
+        recipe_object = Recipe.objects.get(id=recipe_id)
+        if user in recipe_object.like.all():
+            recipe_object.like.remove(user)
+
+        else:
+            recipe_object.like.add(user)
+
+        like_it, create = Favorite.objects.get_or_create(user=user, post_id=recipe_id)
+        if not create:
+            if like_it.value == "Like":
+                like_it.value == "Unlike"
+
+            else:
+                like_it.value == "Like"
+
+        like_it.save()
+
     return redirect("home:recipeView")
 
+
 def submit_recipe(request):
-    if(request.method == "POST"):
+    if (request.method == "POST"):
         recipe_title = request.POST['title']
         recipe_text = request.POST['text']
         newRecipe = Recipe.objects.create();
@@ -28,13 +52,13 @@ def submit_recipe(request):
     else:
         return render(request, 'recipeSubmission.html')
 
+
 class RecipeView(generic.ListView):
     model = Recipe
     template_name = 'recipeView.html'
+
     def get_queryset(self):
         """
         Excludes any questions that aren't published yet.
         """
         return Recipe.objects.all()
-
-
