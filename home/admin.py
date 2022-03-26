7a44c598ba4f9fa8d064bca9b33677a11483f9f6
aljariri <ma7qg@virginia.edu>
2022-03-26 14:05:14 -0400

    Update admin.py

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Recipe, Favorited
+from .models import Recipe, Favorite
 
 admin.site.register(Recipe)
-admin.site.register(Favorited)
+admin.site.register(Favorite)
