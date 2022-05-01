d46b542e24c877e50856f912d092e3391aea837b
anrath <aklounts@gmail.com>
2022-05-01 13:44:43 -0400

    Adding Recipe and Ingredient tests

--- home/tests.py
+++ home/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import Recipe
+from .models import Ingredients, Recipe
 
 # Create your tests here.
 """
@@ -15,3 +15,34 @@ Output: Both the Reuben and Salad can be found by their recipe names,
 Type of Test: Equivalency
 User Story ID: 5
 """
+
+
+"""
+Author: Kasra Lekan
+Input: a recipe model and an ingredient model
+Type of Test: Equivalency
+"""
+class RecipeTestCase(TestCase):
+    def setUp(self):
+        recipe = Recipe.objects.create(recipeTitle="cooked_bacon", recipeText="Cooked Bacon")
+        Ingredients.objects.create(recipe=recipe, name="water", units="mg", quantity="3")
+
+    def test_recipe_text_equivalence(self):
+        cooked_bacon = Recipe.objects.get(recipeTitle="cooked_bacon")
+        self.assertEqual(str(cooked_bacon), 'cooked_bacon')
+        self.assertEqual(cooked_bacon.recipeText, 'Cooked Bacon')
+        
+    def test_recipe_like_count(self):
+        cooked_bacon = Recipe.objects.get(recipeTitle="cooked_bacon")
+        self.assertEqual(cooked_bacon.total_likes(), 0)
+
+    def test_ingredient_text_equivalence(self):
+        water = Ingredients.objects.get(name="water")
+        self.assertEqual(water.name, 'water')
+        self.assertEqual(water.units, 'mg')
+        self.assertEqual(water.quantity, 3)
+        
+    def test_recipe_add_ingredient(self):
+        cooked_bacon = Recipe.objects.get(recipeTitle="cooked_bacon")
+        water = Ingredients.objects.get(name="water")
+        self.assertEqual(water.recipe, cooked_bacon)
